82bb821b35547de591683120c942a9a837899a7c
galiishaq <galiishaq@gmail.com>
2025-04-12 17:45:09 +0000

    Add vectorize_data function for TF-IDF vectorization and move prepare_dataset function to src/preprocessing.py

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -2,10 +2,18 @@ import re
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 nltk.download("stopwords", quiet=True)
 stop_words = set(stopwords.words("english"))
 stemmer = PorterStemmer()
+
+def prepare_dataset(df):
+    df["cleaned_reviews"] = df["review"].apply(clean_text)
+    X = df["cleaned_reviews"]
+    y = df["label"].map({"truthful": 0, "deceptive": 1})
+    
+    return X, y
 
 def clean_text(text):
     """Basic cleaning: lowercasing, punctuation removal, stopword elimination, and stemming."""
@@ -14,3 +22,9 @@ def clean_text(text):
     words = text.split()
     words = [stemmer.stem(word) for word in words if word not in stop_words]
     return " ".join(words)
+
+def vectorize_data(df, max_feats, ngram=1):
+    vectorizer = TfidfVectorizer(max_features=max_feats, ngram_range=(1, ngram))
+    X, y = prepare_dataset(df)
+    X_vec = vectorizer.fit_transform(X)
+    return X_vec, y
